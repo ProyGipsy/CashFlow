bf611beb08e113bef07046e72a3601fbe3758854
Gipsy <proyectogipsy@gmail.com>
2025-04-08 13:47:58 -0400

    Testing flask_mail

--- app.py
+++ app.py
@@ -19,9 +19,27 @@ from receipt_db import (get_db_connection,
 
 app = Flask(__name__)
 
+from flask import Flask
+from flask_mail import Mail, Message
+# Configuración del servidor SMTP
+app.config['MAIL_SERVER'] = os.environ.get('MAIL_SERVER')
+app.config['MAIL_PORT'] = os.environ.get('MAIL_PORT')
+app.config['MAIL_USE_SSL'] = os.environ.get('MAIL_USE_SSL')
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME_2')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+mail = Mail(app)
+
 @app.route('/')
 def index():
-   return render_template('index.html')
+    """
+    msg = Message(subject='Testing Flask Mail',
+                  sender=app.config['MAIL_USERNAME'],
+                  recipients=['proyectogipsy@gmail.com', 'yarimacontreras.ucv@gmail.com'])
+    msg.body = "Prueba de envío de correo desde Aplicación de Recibo"
+    mail.send(msg)
+    return "Message sent!"
+    """
+    return render_template('index.html')
 
 
 # INICIOS DE SESIÓN
